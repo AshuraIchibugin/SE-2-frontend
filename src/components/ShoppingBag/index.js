ac8b645b64116d70e9db6efcf95b544e8a68b0f9
AskatDzhambulov <askat.dzhambulov@iaau.edu.kg>
2020-02-04 22:22:34 +0600

    added cart totals in shopping cart page

--- src/components/ShoppingBag/index.js
+++ src/components/ShoppingBag/index.js
@@ -27,53 +27,85 @@ const ShoppingBag = () => {
   return (
     <section className="shopping">
       <div className="container">
-        <div className="shopping-bag">
-          <div className="shopping-bag__title">
-            <h3 className="shopping-bag__title-name">shopping bag</h3>
+        <div className="shopping-wrap">
+          <div className="shopping-bag">
+            <div className="shopping-bag__title">
+              <h3 className="shopping-bag__title-name">shopping bag</h3>
+            </div>
+            <div className="shopping-bag__sections">
+              {bags.map(bag => (
+                <div className="shopping-bag__section" key={bag.id}>
+                  <div className="shopping-bag__section-info">
+                    <span className="shopping-bag__section-info__count">
+                      0{bag.index}
+                    </span>
+                    <div className="shopping-bag__section-info__image">
+                      {bag.img}
+                    </div>
+                  </div>
+                  <div className="shopping-bag__section-detail">
+                    <div className="shopping-bag__section-detail__title">
+                      <p className="shopping-bag__section-detail__title-name">
+                        {bag.title}
+                      </p>
+                    </div>
+                    <div className="shopping-bag__section-detail__content">
+                      <div className="shopping-bag__section-detail__content-left">
+                        <p>
+                          <span>size:</span>
+                          {bag.size}
+                        </p>
+                        <p>
+                          <span>color:</span>
+                          {bag.color}
+                        </p>
+                        <p>
+                          <span>quantity:</span>
+                          {bag.quantity}
+                        </p>
+                      </div>
+                      <div className="shopping-bag__section-detail__content-right">
+                        <span>${bag.price}</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="shopping-bag__buttons">
+              <button className="shopping-bag__button">
+                continue shopping
+              </button>
+              <button className="shopping-bag__button">Update cart</button>
+            </div>
           </div>
-          <div className="shopping-bag__sections">
-            {bags.map(bag => (
-              <div className="shopping-bag__section" key={bag.id}>
-                <div className="shopping-bag__section-info">
-                  <span className="shopping-bag__section-info__count">
-                    0{bag.index}
-                  </span>
-                  <div className="shopping-bag__section-info__image">
-                    {bag.img}
-                  </div>
-                </div>
-                <div className="shopping-bag__section-detail">
-                  <div className="shopping-bag__section-detail__title">
-                    <p className="shopping-bag__section-detail__title-name">
-                      {bag.title}
-                    </p>
-                  </div>
-                  <div className="shopping-bag__section-detail__content">
-                    <div className="shopping-bag__section-detail__content-left">
-                      <p>
-                        <span>size:</span>
-                        {bag.size}
-                      </p>
-                      <p>
-                        <span>color:</span>
-                        {bag.color}
-                      </p>
-                      <p>
-                        <span>quantity:</span>
-                        {bag.quantity}
-                      </p>
-                    </div>
-                    <div className="shopping-bag__section-detail__content-right">
-                      <span>${bag.price}</span>
-                    </div>
-                  </div>
-                </div>
+          <div className="cart-totals">
+            <div className="cart-totals__title">
+              <h3 className="cart-totals__title-name">cart totals</h3>
+            </div>
+            <div className="cart-totals__section">
+              <div className="cart-totals__section-detail">
+                <span className="cart-totals__section-detail__name">
+                  subtotal
+                </span>
+                <span className="cart-totals__section-detail__price">$344</span>
               </div>
-            ))}
-          </div>
-          <div className="shopping-bag__buttons">
-            <button className="shopping-bag__button">continue shopping</button>
-            <button className="shopping-bag__button">Update cart</button>
+              <div className="cart-totals__section-detail">
+                <span className="cart-totals__section-detail__name">
+                  shipping
+                </span>
+                <span className="cart-totals__section-detail__price">
+                  $0.00
+                </span>
+              </div>
+              <div className="cart-totals__section-detail">
+                <span className="cart-totals__section-detail__name">total</span>
+                <span className="cart-totals__section-detail__price">$344</span>
+              </div>
+              <button className="cart-totals__section-button">
+                Proceed to Checkout
+              </button>
+            </div>
           </div>
         </div>
       </div>
